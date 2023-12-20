b1fa0e8734ed69a112de2e83f9414c0b83396c5d
Isaac Turner <spacey_sooty@outlook.com>
2023-12-20 16:39:47 +0800

    format using rustfmt

--- grapplefrcdriver/src/lib.rs
+++ grapplefrcdriver/src/lib.rs
@@ -4,7 +4,11 @@
 #![allow(deref_nullptr)]
 #![allow(rustdoc::broken_intra_doc_links)]
 
-use std::{cell::RefCell, ffi::{c_char, CString, c_int}, ptr};
+use std::{
+    cell::RefCell,
+    ffi::{c_char, c_int, CString},
+    ptr,
+};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -13,46 +17,48 @@ pub mod lasercan;
 
 // From https://michael-f-bryan.github.io/rust-ffi-guide/errors/return_types.html
 
-thread_local!{
+thread_local! {
   static LAST_ERROR: RefCell<Option<anyhow::Error>> = RefCell::new(None);
 }
 
 pub fn update_last_error(err: anyhow::Error) {
-  LAST_ERROR.with(|prev| {
-      *prev.borrow_mut() = Some(err);
-  });
+    LAST_ERROR.with(|prev| {
+        *prev.borrow_mut() = Some(err);
+    });
 }
 
 /// Retrieve the most recent error, clearing it in the process.
 pub fn take_last_error() -> Option<anyhow::Error> {
-  LAST_ERROR.with(|prev| prev.borrow_mut().take())
+    LAST_ERROR.with(|prev| prev.borrow_mut().take())
 }
 
 pub fn with_err(result: anyhow::Result<()>) -> c_int {
-  match result {
-    Ok(()) => 0,
-    Err(e) => {
-      update_last_error(e);
-      -1
-    },
-  }
+    match result {
+        Ok(()) => 0,
+        Err(e) => {
+            update_last_error(e);
+            -1
+        }
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn last_error() -> *mut c_char {
-  match take_last_error() {
-    Some(err) => {
-      let str = CString::new(format!("{}", err)).unwrap();
-      str.into_raw()
-    },
-    None => ptr::null_mut()
-  }
+    match take_last_error() {
+        Some(err) => {
+            let str = CString::new(format!("{}", err)).unwrap();
+            str.into_raw()
+        }
+        None => ptr::null_mut(),
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn free_error(s: *mut c_char) {
-  if s.is_null() {
-    return;
-  }
-  unsafe { drop(CString::from_raw(s)); }
+    if s.is_null() {
+        return;
+    }
+    unsafe {
+        drop(CString::from_raw(s));
+    }
 }
