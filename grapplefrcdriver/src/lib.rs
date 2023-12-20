095f8ca01953d15335f941b0f9ef90669f2710a3
Isaac Turner <spacey_sooty@outlook.com>
2023-12-20 16:07:25 +0800

    Finished python implementation

--- grapplefrcdriver/src/lib.rs
+++ grapplefrcdriver/src/lib.rs
@@ -4,7 +4,11 @@
 #![allow(deref_nullptr)]
 #![allow(rustdoc::broken_intra_doc_links)]
 
-use std::{cell::RefCell, ffi::{c_char, CString, c_int}, ptr};
+use std::{
+    cell::RefCell,
+    ffi::{c_char, c_int, CString},
+    ptr,
+};
 
 use jni::JNIEnv;
 
@@ -17,77 +21,81 @@ pub mod lasercan;
 
 // From https://michael-f-bryan.github.io/rust-ffi-guide/errors/return_types.html
 
-thread_local!{
+thread_local! {
   static LAST_ERROR: RefCell<Option<anyhow::Error>> = RefCell::new(None);
 }
 
 pub fn update_last_error(err: anyhow::Error) {
-  LAST_ERROR.with(|prev| {
-      *prev.borrow_mut() = Some(err);
-  }); 
+    LAST_ERROR.with(|prev| {
+        *prev.borrow_mut() = Some(err);
+    });
 }
 
 /// Retrieve the most recent error, clearing it in the process.
 pub fn take_last_error() -> Option<anyhow::Error> {
-  LAST_ERROR.with(|prev| prev.borrow_mut().take())
+    LAST_ERROR.with(|prev| prev.borrow_mut().take())
 }
 
 pub fn with_err(result: anyhow::Result<()>) -> c_int {
-  match result {
-    Ok(()) => 0,
-    Err(e) => {
-      update_last_error(e);
-      -1
-    },
-  }
+    match result {
+        Ok(()) => 0,
+        Err(e) => {
+            update_last_error(e);
+            -1
+        }
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn last_error() -> *mut c_char {
-  match take_last_error() {
-    Some(err) => {
-      let str = CString::new(format!("{}", err)).unwrap();
-      str.into_raw()
-    },
-    None => ptr::null_mut()
-  }
+    match take_last_error() {
+        Some(err) => {
+            let str = CString::new(format!("{}", err)).unwrap();
+            str.into_raw()
+        }
+        None => ptr::null_mut(),
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn free_error(s: *mut c_char) {
-  if s.is_null() {
-    return;
-  }
-  unsafe { drop(CString::from_raw(s)); }
+    if s.is_null() {
+        return;
+    }
+    unsafe {
+        drop(CString::from_raw(s));
+    }
 }
 
 #[repr(C)]
 pub enum COptional<T> {
-  None,
-  Some(T)
+    None,
+    Some(T),
 }
 
 impl<T> From<Option<T>> for COptional<T> {
-  fn from(value: Option<T>) -> Self {
-    match value {
-      Some(v) => COptional::Some(v),
-      None => COptional::None,
+    fn from(value: Option<T>) -> Self {
+        match value {
+            Some(v) => COptional::Some(v),
+            None => COptional::None,
+        }
     }
-  }
 }
 
 pub trait JNIResultExtension<T> {
-  fn with_jni_throw<'local, F: FnOnce(T)>(self, env: &mut JNIEnv<'local>, f: F);
+    fn with_jni_throw<'local, F: FnOnce(T)>(self, env: &mut JNIEnv<'local>, f: F);
 }
 
 impl<T> JNIResultExtension<T> for anyhow::Result<T> {
-  fn with_jni_throw<'local, F: FnOnce(T)>(self, env: &mut JNIEnv<'local>, f: F) {
-    match self {
-      Ok(v) => f(v),
-      Err(e) => {
-        let cls = env.find_class("au/grapplerobotics/NativeException").unwrap();
-        env.throw_new(cls, format!("{}", e)).unwrap();
-      },
+    fn with_jni_throw<'local, F: FnOnce(T)>(self, env: &mut JNIEnv<'local>, f: F) {
+        match self {
+            Ok(v) => f(v),
+            Err(e) => {
+                let cls = env
+                    .find_class("au/grapplerobotics/NativeException")
+                    .unwrap();
+                env.throw_new(cls, format!("{}", e)).unwrap();
+            }
+        }
     }
-  }
 }
--- grpl-py/src/lib.rs
+++ grpl-py/src/lib.rs
@@ -1,17 +1,80 @@
+#![allow(dead_code)]
+
 use pyo3::prelude::*;
+use grapplefrcdriver::lasercan::{LaserCanDevice, lasercan_get_status, lasercan_set_range, lasercan_set_timing_budget, lasercan_set_roi};
+use grapple_frc_msgs::grapple::lasercan::{LaserCanStatusFrame, LaserCanRoi};
+use std::ptr::addr_of_mut;
 
 #[pyclass]
+#[derive(PartialEq)]
 enum LaserCanRangingMode {
     Long,
     Short,
 }
 
+impl LaserCanRangingMode {
+    fn is_long(&self) -> bool {
+        if *self == LaserCanRangingMode::Long {
+            return true
+        }
+        false
+    }
+}
+
 #[pyclass]
 enum LaserCanTimingBudget {
-    TimingBudget20ms = 20,
-    TimingBudget33ms = 33,
-    TimingBudget50ms = 50,
-    TimingBudget100ms = 100,
+    TimingBudget20ms,
+    TimingBudget33ms,
+    TimingBudget50ms,
+    TimingBudget100ms,
+}
+
+impl LaserCanTimingBudget {
+    fn as_u8(&self) -> u8 {
+        match self {
+            LaserCanTimingBudget::TimingBudget20ms => 20,
+            LaserCanTimingBudget::TimingBudget33ms => 33,
+            LaserCanTimingBudget::TimingBudget50ms => 50,
+            LaserCanTimingBudget::TimingBudget100ms => 100,
+        }
+    }
+}
+
+#[pyclass]
+struct LaserCAN {
+    can_id: u8,
+    handle: LaserCanDevice,
+}
+
+impl LaserCAN {
+
+    fn new(can_id: u8) -> Self {
+        let handle = LaserCanDevice::new(can_id);
+        LaserCAN {
+            handle,
+            can_id,
+        }
+    }
+
+    fn get_measurement(&mut self) -> Option<LaserCanStatusFrame> {
+        let status = lasercan_get_status(addr_of_mut!(self.handle));
+        if  status.status != 0xFF {
+            return Some(status)
+        }
+        None
+    }
+
+    fn set_ranging_mode(&mut self, mode : LaserCanRangingMode) -> i32 {
+        lasercan_set_range(addr_of_mut!(self.handle), mode.is_long())
+    }
+
+    fn set_timing_range_budget(&mut self, budget: LaserCanTimingBudget) -> i32 {
+        lasercan_set_timing_budget(addr_of_mut!(self.handle), budget.as_u8())
+    }
+
+    fn set_roi(&mut self, roi: LaserCanRoi) -> i32 {
+        lasercan_set_roi(addr_of_mut!(self.handle), roi)
+    }
 }
 
 #[pymodule]
@@ -24,6 +87,7 @@ fn grpl(_py: Python, m: &PyModule) -> PyResult<()> {
 
     m.add_class::<LaserCanRangingMode>()?;
     m.add_class::<LaserCanTimingBudget>()?;
+    m.add_class::<LaserCAN>()?;
 
     Ok(())
 }
