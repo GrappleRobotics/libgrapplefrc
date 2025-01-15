853a58a24b40a4a9ebbba6ee07ae87924f1e4f68
Jaci Brunning <jaci.brunning@gmail.com>
2025-01-15 16:16:26 +1100

    Update to 2025.0.2

--- grapplefrcdriver/build.py
+++ grapplefrcdriver/build.py
@@ -6,7 +6,7 @@ import zipfile
 NEW_PATH = os.getenv("PATH") + ";" + os.path.expanduser("~/.gradle/toolchains/frc/2025/roborio/bin")
 
 # TODO: Load from cargo metadata
-VERSION = "2025.0.0"
+VERSION = "2025.0.2"
 
 def run(*cmd):
   env = os.environ.copy()
